73e47c277dbcf6cdf744072426019c10715bcd53
Kaleb Ercanbrack <twitcherc@gmail.com>
2019-01-15 10:25:09 -0700

    added change folder

---
+++ change/change.js
@@ -0,0 +1,6 @@
+
+(function(){
+    document.getElementById("container").style.backgroundColor = "red";
+    document.getElementById("example").style.backgroundColor = "blue";
+    document.getElementById("example").innerHTML = "Hooray! You have added javascript to your page! Good Job!";
+})();
