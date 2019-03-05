b813750f1aea8927ca5e0b9e1a6db0f70c854116
kaleb <twitcherc@gmail.com>
2019-03-05 10:29:24 -0700

    03-05 update

--- Javascript/03-05/main.js
+++ Javascript/03-05/main.js
@@ -2,9 +2,16 @@ function loadDoc(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+            let myresult = JSON.parse(this.responseText);
+            console.log(myresult);
+            $(".storetitle").html(myresult.value);
+            // for(let i = 0; i < myresult.StoreItems.length; i++){
+            //     $(".storetitle").append(`<div>${myresult.StoreItems[i].itemName}: ${myresult.StoreItems[i].Price}</div>`);
+            // }
+            
         }
     };
-    xhttp.open("get", "ajax_info.txt", true);
+    xhttp.open("get", "https://api.chucknorris.io/jokes/random?category=dev", true);
     xhttp.send();
+    
 }
